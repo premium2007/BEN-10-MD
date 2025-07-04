e55be922660f63934f58b71675234cfd0d01e678
premium2007 <Antoniochin867@gmail.com>
2025-07-04 13:33:04 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: '𝗕𝗘𝗡 10 𝗠𝗗',
   author: '‎',
   botName: "𝗕𝗘𝗡 10 𝗠𝗗",
-  botOwner: '103', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: '103', // PRIME
+  ownerNumber: '18765942612', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
